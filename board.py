285d9015edd125349b66bf2cc9b76b5959131f3b
Karan Vombatkere <kvombatkere@gmail.com>
2020-05-05 01:30:37 -0400

    Started Hex Class

---
+++ board.py
@@ -0,0 +1,20 @@
+#Karan Vombatkere
+#May 2020
+
+##Class to implement Catan board Hexagonal Tile
+
+from string import *
+import numpy as np
+
+class hexTile():
+    'Class Definition for Catan Board Hexagonal Tile'
+
+    #Object Creation
+    def __init__(self, resourceName, num):
+        self.resource = resourceName
+        self.num = num
+
+    #Function to Display Hex Info
+    def displayHexInfo(self):
+        print(self.resource, self.num)
+    
