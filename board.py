9340817614b8f3fceb1e957567c54e4540b859d9
Karan Vombatkere <kvombatkere@gmail.com>
2020-05-07 18:25:17 -0400

    added namedtuples

--- board.py
+++ board.py
@@ -1,20 +1,30 @@
 #Karan Vombatkere
 #May 2020
 
-##Class to implement Catan board
-##Use a graph representation for the board
-
+#Imports
 from string import *
 import numpy as np
+from hexTile import *
+#import pygame
 
+###Class to implement Catan board
+##Use a graph representation for the board
 class catanBoard():
     'Class Definition for Catan Board '
-    
     #Object Creation - creates a random board configuration with hexTiles
     #Takes
     def __init__(self):
         self.HexTileList = [] #Dictionary to store all hextiles
-        self.randResourcesList = self.getRandomResourceList()
+        self.resourcesList = self.getRandomResourceList()
+
+        #Get a random permutation of indices 0-18 to use with the resource list
+        randomIndices = np.random.permutation([i for i in range(len(self.resourcesList))])
+        print(randomIndices)
+
+        #Generate the hexes and the graphs with the Centers and Resources defined
+        for rand_i in randomIndices:
+            #Get the list of neighbors for this hexTile
+            newHexTile = hexTile(self.resourcesList[rand_i], Point(2,3)
         #Define the central Hex
         #hex0 = hexTile()
 
@@ -26,7 +36,7 @@ class catanBoard():
 
     #Function to generate a random permutation of resources
     def getRandomResourceList(self):
-        #Define Resources as a Global Dictionary
+        #Define Resources as a dict
         Resource_Dict = {'DESERT':1, 'ORE':3, 'BRICK':3, 'WHEAT':4, 'WOOD':4, 'SHEEP':4}
         #Get a random permutation of the numbers
         NumberList = np.random.permutation([2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12])
@@ -37,16 +47,14 @@ class catanBoard():
             numberofResource = Resource_Dict[r]
             if(r != 'DESERT'):
                 for n in range(numberofResource):
-                    resourceList.append({r:NumberList[numIndex]})
+                    resourceList.append(Resource(r, NumberList[numIndex]))
                     numIndex += 1
             else:
-                resourceList.append({r:None})
+                resourceList.append(Resource(r, None))
 
-        #Get a random permutation of resources
-        resourceListRand = np.random.permutation(resourceList)
-        print(resourceListRand)
+        print(resourceList)
 
-        return resourceListRand
+        return resourceList
 
     #Function to Display Hex Info
     def displayBoard(self):
--- hexTile.py
+++ hexTile.py
@@ -1,23 +1,45 @@
 #Karan Vombatkere
 #May 2020
 
+#IMPORTS
+import collections
+
 ##Class to implement Catan board Hexagonal Tile
+Point = collections.namedtuple("Point", ["x", "y"])
+Resource = collections.namedtuple("Resource", ["type", "num"])
 
 class hexTile():
     'Class Definition for Catan Board Hexagonal Tile'
 
-    #Object Creation
-    def __init__(self, resourceName, num):
-        self.resource = resourceName
-        self.num = num
+    #Object Creation - specify the resource, num, center and neighbor list
+    #Center is a point and neighborList is a list of hexTiles
+    def __init__(self, hexResource, center, neighborList=None):
+        self.resource = hexResource
+        self.coord = center
+        self.neighborList = neighborList
+
+    #Function to update hex neighbors
+    def updateNeighbors(self):
+        
+
 
     #Function to Display Hex Info
     def displayHexInfo(self):
-        print(self.resource, self.num)
+        print('Resource:{}; Center:{}'.format(self.resource, self.coord))
+        return None
+        
 
+    #Function to display Hex Neighbors
+    def displayHexNeighbors(self):
+        print('Neighbors:')
+        for neighbor in self.neighborList:
+            neighbor.displayHexInfo()
+
+        return None
 
     
 
 #Test Code
-testHex = hexTile('Ore', 8)
-testHex.displayHexInfo()
+# testHex = hexTile(Resource('Ore', 8), Point(2,3), [hexTile(Resource('Wheat', 11), Point(5,6)), hexTile(Resource('Brick', 11), Point(7,4))])
+# testHex.displayHexInfo()
+# testHex.displayHexNeighbors()
