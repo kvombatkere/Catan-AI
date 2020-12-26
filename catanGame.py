c9a1b748b378001ff1543f7439b5c968bf774d05
Karan Vombatkere <kvombatkere@gmail.com>
2020-12-26 18:13:04 -0500

    gameView updates

--- catanGame.py
+++ catanGame.py
@@ -246,16 +246,17 @@ class catanGame():
                                     if(diceRolled == False): #Only roll dice once
                                         diceNum = self.rollDice()
                                         diceRolled = True
-
+                                        
+                                        self.boardView.displayDiceRoll(diceNum)
                                         #Code to update player resources with diceNum
                                         self.update_playerResources(diceNum, currPlayer)
 
                                 #Check if player wants to build road
-                                if(self.buildRoad_button.collidepoint(e.pos)):
+                                if(self.boardView.buildRoad_button.collidepoint(e.pos)):
                                     #Code to check if road is legal and build
                                     if(diceRolled == True): #Can only build after rolling dice
-                                        self.buildRoad_display(currPlayer)
-                                        self.displayGameScreen(None, None)#Update back to original gamescreen
+                                        self.boardView.buildRoad_display(currPlayer)
+                                        self.boardView.displayGameScreen(None, None)#Update back to original gamescreen
 
                                         #Check if player gets longest road and update Victory points
                                         self.check_longest_road(currPlayer)
@@ -263,23 +264,23 @@ class catanGame():
                                         print("Player:{}, Resources:{}, Points: {}".format(currPlayer.name, currPlayer.resources, currPlayer.victoryPoints))
 
                                 #Check if player wants to build settlement
-                                if(self.buildSettlement_button.collidepoint(e.pos)):
+                                if(self.boardView.buildSettlement_button.collidepoint(e.pos)):
                                     if(diceRolled == True): #Can only build settlement after rolling dice
-                                        self.buildSettlement_display(currPlayer)
-                                        self.displayGameScreen(None, None)#Update back to original gamescreen
+                                        self.boardView.buildSettlement_display(currPlayer)
+                                        self.boardView.displayGameScreen(None, None)#Update back to original gamescreen
                                         #Show updated points and resources  
                                         print("Player:{}, Resources:{}, Points: {}".format(currPlayer.name, currPlayer.resources, currPlayer.victoryPoints))
 
                                 #Check if player wants to build city
-                                if(self.buildCity_button.collidepoint(e.pos)):
+                                if(self.boardView.buildCity_button.collidepoint(e.pos)):
                                     if(diceRolled == True): #Can only build city after rolling dice
-                                        self.buildCity_display(currPlayer)
-                                        self.displayGameScreen(None, None)#Update back to original gamescreen
+                                        self.boardView.buildCity_display(currPlayer)
+                                        self.boardView.displayGameScreen(None, None)#Update back to original gamescreen
                                         #Show updated points and resources  
                                         print("Player:{}, Resources:{}, Points: {}".format(currPlayer.name, currPlayer.resources, currPlayer.victoryPoints))
 
                                 #Check if player wants to draw a development card
-                                if(self.devCard_button.collidepoint(e.pos)):
+                                if(self.boardView.devCard_button.collidepoint(e.pos)):
                                     if(diceRolled == True): #Can only draw devCard after rolling dice
                                         currPlayer.draw_devCard(self.board)
                                         #Show updated points and resources  
@@ -287,9 +288,9 @@ class catanGame():
                                         print('Available Dev Cards:', currPlayer.devCards)
 
                                 #Check if player wants to play a development card - can play devCard whenever after rolling dice
-                                if(self.playDevCard_button.collidepoint(e.pos)):
+                                if(self.boardView.playDevCard_button.collidepoint(e.pos)):
                                         currPlayer.play_devCard(self)
-                                        self.displayGameScreen(None, None)#Update back to original gamescreen
+                                        self.boardView.displayGameScreen(None, None)#Update back to original gamescreen
                                         #Check for Largest Army 
                                         self.check_largest_army(currPlayer)
                                         #Show updated points and resources  
@@ -297,7 +298,7 @@ class catanGame():
                                         print('Available Dev Cards:', currPlayer.devCards)
 
                                 #Check if player wants to end turn
-                                if(self.endTurn_button.collidepoint(e.pos)):
+                                if(self.boardView.endTurn_button.collidepoint(e.pos)):
                                     if(diceRolled == True): #Can only end turn after rolling dice
                                         print("Ending Turn!")
                                         turnOver = True  #Update flag to nextplayer turn
--- gameView.py
+++ gameView.py
@@ -11,23 +11,15 @@ pygame.init()
 class catanGameView():
     'Class definition for Catan board display'
     def __init__(self, catanBoardObject, catanGameObject):
-        # self.hexTileDict = {} #Dict to store all hextiles, with hexIndex as key
-        # self.vertex_index_to_pixel_dict = {} #Dict to store the Vertices coordinates with vertex indices as keys
-        # self.boardGraph = {} #Dict to store the vertex objects with the pixelCoordinates as keys
-        # self.resourcesList = self.getRandomResourceList()
-
         self.board = catanBoardObject
         self.game = catanGameObject
 
-        #Use pygame to display the board
-        self.edgeLength = 80 #Specify for hex size
-
-        self.size = self.width, self.height = 1000, 800
-        self.flat = Layout(layout_flat, Point(self.edgeLength, self.edgeLength), Point(self.width/2, self.height/2)) #specify Layout
-
-        self.screen = pygame.display.set_mode(self.size)
+        # #Use pygame to display the board
+        self.screen = pygame.display.set_mode(self.board.size)
         pygame.display.set_caption('Settlers of Catan')
         self.font_resource = pygame.font.SysFont('cambria', 15)
+        self.font_ports = pygame.font.SysFont('cambria', 12)
+
 
         self.font_button = pygame.font.SysFont('cambria', 12)
         self.font_diceRoll = pygame.font.SysFont('cambria', 25) #dice font
@@ -40,17 +32,17 @@ class catanGameView():
     def displayInitialBoard(self):
         #Dictionary to store RGB Color values
         colorDict_RGB = {"BRICK":(255,51,51), "ORE":(128, 128, 128), "WHEAT":(255,255,51), "WOOD":(0,153,0), "SHEEP":(51,255,51), "DESERT":(255,255,204)}
-        pygame.draw.rect(self.screen, pygame.Color('royalblue2'), (0,0,self.width,self.height)) #blue background
+        pygame.draw.rect(self.screen, pygame.Color('royalblue2'), (0,0,self.board.width, self.board.height)) #blue background
 
         #Render each hexTile
         for hexTile in self.board.hexTileDict.values():
-            hexTileCorners = polygon_corners(self.flat, hexTile.hex)
+            hexTileCorners = polygon_corners(self.board.flat, hexTile.hex)
 
             hexTileColor_rgb = colorDict_RGB[hexTile.resource.type]
-            pygame.draw.polygon(self.screen, pygame.Color(hexTileColor_rgb[0],hexTileColor_rgb[1], hexTileColor_rgb[2]), hexTileCorners, self.width==0)
+            pygame.draw.polygon(self.screen, pygame.Color(hexTileColor_rgb[0],hexTileColor_rgb[1], hexTileColor_rgb[2]), hexTileCorners, self.board.width==0)
             #print(hexTile.index, hexTileCorners)
 
-            hexTile.pixelCenter = hex_to_pixel(self.flat, hexTile.hex) #Get pixel center coordinates of hex
+            hexTile.pixelCenter = hex_to_pixel(self.board.flat, hexTile.hex) #Get pixel center coordinates of hex
             if(hexTile.resource.type != 'DESERT'): #skip desert text/number
                 resourceText = self.font_resource.render(str(hexTile.resource.type) + " (" +str(hexTile.resource.num) + ")", False, (0,0,0))
                 self.screen.blit(resourceText, (hexTile.pixelCenter.x -25, hexTile.pixelCenter.y)) #add text to hex
@@ -59,7 +51,7 @@ class catanGameView():
         #Display the Ports
         for vCoord, vertexInfo in self.board.boardGraph.items():
             if(vertexInfo.port != False):
-                portText = self.font_resource.render(vertexInfo.port + " PORT", False, (230,0,0))
+                portText = self.font_ports.render(vertexInfo.port, False, (230,0,0))
                 self.screen.blit(portText, (vCoord.x, vCoord.y)) 
             
         pygame.display.update()
