0f072f0b6aa180266dc93ad7bc2ba6d82b71d188
Karan Vombatkere <kvombatkere@gmail.com>
2020-12-26 22:39:41 -0500

    added class for modelState

---
+++ code/modelState.py
@@ -0,0 +1,14 @@
+#Karan Vombatkere
+#Settlers of Catan, 2020
+
+#Imports
+from board import *
+from game import *
+from player import *
+from AIPlayer import *
+
+class modelState():
+    '''Define the variables needed by the RL model using a state object'''
+    __init__(self):
+        
+
